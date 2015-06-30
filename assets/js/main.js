a9fd8acb5af6c315118a65d071b06b9f1b7e5ca7
Ihor Orlovskyi <ihor.droid@gmail.com>
2015-06-30 08:25:28 +0300

    refactor previous code, add imgs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-$( document ).ready(function() {
-
-  //
-
-});
-
 /**
  * Created by Sallar Kaboli <sallar.kaboli@gmail.com>
  * @sallar
@@ -23,11 +17,11 @@ $( document ).ready(function() {
  * Cache
  */
 var $content = $('header .content')
-  , $blur    = $('header .overlay')
-  , wHeight  = $(window).height();
+	, $blur    = $('header .overlay')
+	, wHeight  = $(window).height();
 
 $(window).on('resize', function(){
-  wHeight = $(window).height();
+	wHeight = $(window).height();
 });
 
 /**
@@ -35,12 +29,12 @@ $(window).on('resize', function(){
  */
 window.requestAnimFrame = (function()
 {
-  return  window.requestAnimationFrame       ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame    ||
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
+	return  window.requestAnimationFrame       ||
+					window.webkitRequestAnimationFrame ||
+					window.mozRequestAnimationFrame    ||
+					function( callback ){
+						window.setTimeout(callback, 1000 / 60);
+					};
 })();
 
 /**
@@ -48,59 +42,59 @@ window.requestAnimFrame = (function()
  */
 function Scroller()
 {
-  this.latestKnownScrollY = 0;
-  this.ticking            = false;
+	this.latestKnownScrollY = 0;
+	this.ticking            = false;
 }
 
 Scroller.prototype = {
-  /**
-   * Initialize
-   */
-  init: function() {
-    window.addEventListener('scroll', this.onScroll.bind(this), false);
-  },
+	/**
+	 * Initialize
+	 */
+	init: function() {
+		window.addEventListener('scroll', this.onScroll.bind(this), false);
+	},
 
-  /**
-   * Capture Scroll
-   */
-  onScroll: function() {
-    this.latestKnownScrollY = window.scrollY;
-    this.requestTick();
-  },
+	/**
+	 * Capture Scroll
+	 */
+	onScroll: function() {
+		this.latestKnownScrollY = window.scrollY;
+		this.requestTick();
+	},
 
-  /**
-   * Request a Tick
-   */
-  requestTick: function() {
-    if( !this.ticking ) {
-      window.requestAnimFrame(this.update.bind(this));
-    }
-    this.ticking = true;
-  },
+	/**
+	 * Request a Tick
+	 */
+	requestTick: function() {
+		if( !this.ticking ) {
+			window.requestAnimFrame(this.update.bind(this));
+		}
+		this.ticking = true;
+	},
 
-  /**
-   * Update.
-   */
-  update: function() {
-    var currentScrollY = this.latestKnownScrollY;
-    this.ticking       = false;
-    
-    /**
-     * Do The Dirty Work Here
-     */
-    var slowScroll = currentScrollY / 4
-      , blurScroll = currentScrollY * 2;
-    
-    $content.css({
-      'transform'         : 'translateY(-' + slowScroll + 'px)',
-      '-moz-transform'    : 'translateY(-' + slowScroll + 'px)',
-      '-webkit-transform' : 'translateY(-' + slowScroll + 'px)'
-    });
-    
-    $blur.css({
-      'opacity' : blurScroll / wHeight
-    });
-  }
+	/**
+	 * Update.
+	 */
+	update: function() {
+		var currentScrollY = this.latestKnownScrollY;
+		this.ticking       = false;
+		
+		/**
+		 * Do The Dirty Work Here
+		 */
+		var slowScroll = currentScrollY / 4
+			, blurScroll = currentScrollY * 2;
+		
+		$content.css({
+			'transform'         : 'translateY(-' + slowScroll + 'px)',
+			'-moz-transform'    : 'translateY(-' + slowScroll + 'px)',
+			'-webkit-transform' : 'translateY(-' + slowScroll + 'px)'
+		});
+		
+		$blur.css({
+			'opacity' : blurScroll / wHeight
+		});
+	}
 };
 
 /**
@@ -108,3 +102,19 @@ Scroller.prototype = {
  */
 var scroller = new Scroller();  
 scroller.init();
+
+
+
+
+// Isotope
+var $container = $('.isotope-container');
+$(document).ready(function() {
+	$container.imagesLoaded( function() {
+		$container.isotope({
+			itemSelector: '.isotope-item',
+			layoutMode: 'fitRows',
+		});
+	});
+});
+
+
