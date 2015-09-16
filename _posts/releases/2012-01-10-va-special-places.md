58bb7bd2d01f19a0ae4e006734eca301ea714ddf
Igor Orlovskiy <i_orlovskiy@wargaming.net>
2015-09-16 19:19:07 +0300

    jquery gallary e[periments

--- _posts/releases/2012-01-10-va-special-places.md
+++ _posts/releases/2012-01-10-va-special-places.md
@@ -26,7 +26,7 @@ bandcamp-lable-track:
 discogs: http://www.discogs.com/AstroPilot-Special-Places/release/3517621
 ---
 
-Tracklist:
+<!-- Tracklist:
 
 01. K.D. Expression - Alpae-3717 [100bpm]
 02. Cygna - Camaras De Lamento [110bpm]
@@ -39,4 +39,21 @@ Tracklist:
 09. Field Rotation - Disappear [130bpm]
 10. Nexar - Jewel (AstroPilot Edit) [85bpm]
 11. 36 - A Final Thought [119bpm]
-12. Eguana - A Moment Of Madness [80bpm]
+12. Eguana - A Moment Of Madness [80bpm] -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-4">
+
+            <div class="fotorama" data-width="700" data-ratio="700/467" data-max-width="100%" data-allowfullscreen="true" data-nav="thumbs" data-hash="true">
+                <img data-caption="Lorem ipsum dolor sit amet" src="{{ site.baseurl }}/assets/img/releases/{{ page:release-id }}-hd.jpg" alt="...">
+                <img data-caption="onsectetur adipisicing elit Placeat " src="{{ site.baseurl }}/assets/img/gallery/g1.jpg" alt="...">
+                <img data-caption="numquam est voluptas error quae atque au" src="{{ site.baseurl }}/assets/img/gallery/g2.jpg" alt="...">
+                <img data-caption="sint totam eveniet inventore of" src="{{ site.baseurl }}/assets/img/gallery/g3.jpg" alt="...">
+                <img data-caption="consectetur sapiente pjoop" src="{{ site.baseurl }}/assets/img/gallery/g4.jpg" alt="...">
+                <img data-caption="simus in suscipit velit, fugit veniam n" src="{{ site.baseurl }}/assets/img/gallery/g5.jpg" alt="...">
+            </div>
+
+        </div>
+    </div>
+</div>
--- assets/js/main.js
+++ assets/js/main.js
@@ -17,11 +17,11 @@
  * Cache
  */
 var $content = $('header .content')
-	, $blur    = $('header .overlay')
-	, wHeight  = $(window).height();
+    , $blur    = $('header .overlay')
+    , wHeight  = $(window).height();
 
 $(window).on('resize', function(){
-	wHeight = $(window).height();
+    wHeight = $(window).height();
 });
 
 /**
@@ -29,12 +29,12 @@ $(window).on('resize', function(){
  */
 window.requestAnimFrame = (function()
 {
-	return  window.requestAnimationFrame       ||
-					window.webkitRequestAnimationFrame ||
-					window.mozRequestAnimationFrame    ||
-					function( callback ){
-						window.setTimeout(callback, 1000 / 60);
-					};
+    return  window.requestAnimationFrame       ||
+                    window.webkitRequestAnimationFrame ||
+                    window.mozRequestAnimationFrame    ||
+                    function( callback ){
+                        window.setTimeout(callback, 1000 / 60);
+                    };
 })();
 
 /**
@@ -42,59 +42,59 @@ window.requestAnimFrame = (function()
  */
 function Scroller()
 {
-	this.latestKnownScrollY = 0;
-	this.ticking            = false;
+    this.latestKnownScrollY = 0;
+    this.ticking            = false;
 }
 
 Scroller.prototype = {
-	/**
-	 * Initialize
-	 */
-	init: function() {
-		window.addEventListener('scroll', this.onScroll.bind(this), false);
-	},
+    /**
+     * Initialize
+     */
+    init: function() {
+        window.addEventListener('scroll', this.onScroll.bind(this), false);
+    },
 
-	/**
-	 * Capture Scroll
-	 */
-	onScroll: function() {
-		this.latestKnownScrollY = window.scrollY;
-		this.requestTick();
-	},
+    /**
+     * Capture Scroll
+     */
+    onScroll: function() {
+        this.latestKnownScrollY = window.scrollY;
+        this.requestTick();
+    },
 
-	/**
-	 * Request a Tick
-	 */
-	requestTick: function() {
-		if( !this.ticking ) {
-			window.requestAnimFrame(this.update.bind(this));
-		}
-		this.ticking = true;
-	},
+    /**
+     * Request a Tick
+     */
+    requestTick: function() {
+        if( !this.ticking ) {
+            window.requestAnimFrame(this.update.bind(this));
+        }
+        this.ticking = true;
+    },
 
-	/**
-	 * Update.
-	 */
-	update: function() {
-		var currentScrollY = this.latestKnownScrollY;
-		this.ticking       = false;
-		
-		/**
-		 * Do The Dirty Work Here
-		 */
-		var slowScroll = currentScrollY / 4
-			, blurScroll = currentScrollY * 2;
-		
-		$content.css({
-			'transform'         : 'translateY(-' + slowScroll + 'px)',
-			'-moz-transform'    : 'translateY(-' + slowScroll + 'px)',
-			'-webkit-transform' : 'translateY(-' + slowScroll + 'px)'
-		});
-		
-		$blur.css({
-			'opacity' : blurScroll / wHeight
-		});
-	}
+    /**
+     * Update.
+     */
+    update: function() {
+        var currentScrollY = this.latestKnownScrollY;
+        this.ticking       = false;
+        
+        /**
+         * Do The Dirty Work Here
+         */
+        var slowScroll = currentScrollY / 4
+            , blurScroll = currentScrollY * 2;
+        
+        $content.css({
+            'transform'         : 'translateY(-' + slowScroll + 'px)',
+            '-moz-transform'    : 'translateY(-' + slowScroll + 'px)',
+            '-webkit-transform' : 'translateY(-' + slowScroll + 'px)'
+        });
+        
+        $blur.css({
+            'opacity' : blurScroll / wHeight
+        });
+    }
 };
 
 /**
@@ -109,12 +109,74 @@ scroller.init();
 // Isotope
 var $container = $('.isotope-container');
 $(document).ready(function() {
-	$container.imagesLoaded( function() {
-		$container.isotope({
-			itemSelector: '.isotope-item',
-			layoutMode: 'fitRows',
-		});
-	});
+    $container.imagesLoaded( function() {
+        $container.isotope({
+            itemSelector: '.isotope-item',
+            layoutMode: 'fitRows',
+        });
+    });
 });
 
 
+// Slick
+$(document).ready(function(){
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      fade: true,
+      dots: false,
+      asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: false,
+      centerMode: false,
+      focusOnSelect: true
+    });
+});
+
+
+
+$(document).ready(function() {
+    $('.popup-with-zoom-anim').magnificPopup({
+      type: 'inline',
+
+      fixedContentPos: false,
+      fixedBgPos: true,
+
+      overflowY: 'auto',
+
+      closeBtnInside: true,
+      preloader: false,
+      
+      midClick: true,
+      removalDelay: 300,
+      mainClass: 'my-mfp-zoom-in'
+    });
+});
+
+var groups = {};
+$('.galleryItem').each(function() {
+  var id = parseInt($(this).attr('data-group'), 10);
+  
+  if(!groups[id]) {
+    groups[id] = [];
+  } 
+  
+  groups[id].push( this );
+});
+
+
+$.each(groups, function() {
+  
+  $(this).magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+      closeBtnInside: false,
+      gallery: { enabled:true }
+  })
+  
+});
