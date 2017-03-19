a58e6bff55e979a63342194c8f6d4c88432dacce
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-03-19 05:47:14 +0200

    continue styling

--- _components/ReleasesListSwiper/index.js
+++ _components/ReleasesListSwiper/index.js
@@ -11,6 +11,7 @@ var swiper = new Swiper ('.swiper-container', {
   // slidesPerView: 'auto',
   centeredSlides: true,
   // spaceBetween: 30,
+  slideToClickedSlide: true,
   mousewheelControl: true,
   freeMode: true,
   slidesPerView: 7,
