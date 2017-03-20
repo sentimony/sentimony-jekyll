b032c0e97f3efc67d8e9a3c3a8fea162a458e89b
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-03-20 21:02:57 +0200

    rework js erorr on index at ReleasesListSwiper, fix iframe for safari at ReleasePage

--- _components/ReleasesListSwiper/index.js
+++ _components/ReleasesListSwiper/index.js
@@ -1,47 +1,52 @@
-var swiper = new Swiper ('.swiper-container', {
-  // loop: true,
-  // grabCursor: true,
-  // paginationType: 'progress',
-  // preloadImages: true,
-  // lazyLoading: true,
-  nextButton: '.swiper-button-next',
-  prevButton: '.swiper-button-prev',
-  // pagination: '.swiper-pagination',
-  // paginationClickable: true,
-  centeredSlides: true,
-  // spaceBetween: 30,
-  scrollbar: '.swiper-scrollbar',
-  slideToClickedSlide: true,
-  mousewheelControl: true,
-  mousewheelForceToAxis: true,
-  freeMode: true,
-  slidesPerView: 'auto',
-  // slidesPerView: 7,
-  // breakpoints: {
-  //   1024: {
-  //     slidesPerView: 5
-  //   },
-  //   768: {
-  //     slidesPerView: 4,
-  //   },
-  //   640: {
-  //     slidesPerView: 2
-  //   },
-  //   320: {
-  //     slidesPerView: 1
-  //   }
-  // }
-})
+;$(document).ready(function () {
 
-swiper.slideTo(getSlideIndexByClass('is-selected'), false);
+  if ($('.swiper-container').length) {
+    var swiper = new Swiper ('.swiper-container', {
+      // loop: true,
+      // grabCursor: true,
+      // paginationType: 'progress',
+      // preloadImages: true,
+      // lazyLoading: true,
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      // pagination: '.swiper-pagination',
+      // paginationClickable: true,
+      centeredSlides: true,
+      // spaceBetween: 30,
+      scrollbar: '.swiper-scrollbar',
+      slideToClickedSlide: true,
+      mousewheelControl: true,
+      mousewheelForceToAxis: true,
+      freeMode: true,
+      slidesPerView: 'auto',
+      // slidesPerView: 7,
+      // breakpoints: {
+      //   1024: {
+      //     slidesPerView: 5
+      //   },
+      //   768: {
+      //     slidesPerView: 4,
+      //   },
+      //   640: {
+      //     slidesPerView: 2
+      //   },
+      //   320: {
+      //     slidesPerView: 1
+      //   }
+      // }
+    })
 
-function getSlideIndexByClass(className) {
-  var index = 0;
-  $.each($('.swiper-wrapper').children(), function(i, item) {
-    if ($(item).hasClass(className)) {
-      index = i;
-      return false;
+    swiper.slideTo(getSlideIndexByClass('is-selected'), false);
+
+    function getSlideIndexByClass(className) {
+      var index = 0;
+      $.each($('.swiper-wrapper').children(), function(i, item) {
+        if ($(item).hasClass(className)) {
+          index = i;
+          return false;
+        }
+      });
+      return index;
     }
-  });
-  return index;
-}
+  }
+});
