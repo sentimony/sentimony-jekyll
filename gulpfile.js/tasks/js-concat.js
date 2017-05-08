4786f7193abd459fcfae6daa3a3323e6fef05959
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-05-08 20:45:55 +0300

    cleaning, rework svg-icons

--- gulpfile.js/tasks/js-concat.js
+++ gulpfile.js/tasks/js-concat.js
@@ -3,7 +3,6 @@ var gulp = require('gulp'),
 
 gulp.task('js-concat', ['sass'], function () {
   return gulp.src([
-    // 'node_modules/jquery/dist/jquery.min.js',
     'node_modules/swiper/dist/js/swiper.min.js',
     'node_modules/goodshare.js/goodshare.min.js',
     'node_modules/lazysizes/lazysizes.min.js',
