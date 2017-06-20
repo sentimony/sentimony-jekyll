4a7b3c464a241f11e2a453c7b618347e7c049a6e
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-06-21 00:00:40 +0300

    upd gulp js-concat

--- gulpfile.js/tasks/js-concat.js
+++ gulpfile.js/tasks/js-concat.js
@@ -6,7 +6,7 @@ gulp.task('js-concat', ['js-copy'], function () {
       'node_modules/vue/dist/vue.min.js',
       'node_modules/vue-nav-tabs/dist/vue-tabs.js',
       'node_modules/swiper/dist/js/swiper.min.js',
-      'node_modules/goodshare.js/goodshare.min.js',
+      // 'node_modules/goodshare.js/goodshare.min.js',
       'node_modules/lazysizes/lazysizes.min.js',
       '_js/vue-tabs.init.js',
       '_components/**/*.js'
