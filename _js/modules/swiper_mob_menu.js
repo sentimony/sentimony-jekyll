7c14b43d0cf5a83afab31ae5fe1eddcd1a67825e
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-07-05 09:26:05 +0300

    upd mob menu

--- _js/modules/swiper_mob_menu.js
+++ _js/modules/swiper_mob_menu.js
@@ -1,44 +1,17 @@
 define('swiper_mob_menu', ['swiper'], function (Swiper) {
   'use strict';
 
-    var swiperMobMenu = new Swiper('.swiper-container.js-swiper-mob-menu', {
-      nextButton: '.js-swiper-mob-menu-open',
-      prevButton: '.js-swiper-mob-menu-close',
-      onlyExternal: true,
-      // slideToClickedSlide: true,
-      breakpoints: {
-        754: {
-          onlyExternal: false,
-          // shortSwipes: false,
-        }
+  var swiperMobMenu = new Swiper('.swiper-container.js-swiper-mob-menu', {
+    nextButton: '.js-swiper-mob-menu-open',
+    prevButton: '.js-swiper-mob-menu-close',
+    onlyExternal: true,
+    slidesPerView: 'auto',
+    // slideToClickedSlide: true,
+    breakpoints: {
+      754: {
+        onlyExternal: false,
+        // shortSwipes: false,
       }
-    })
-
-    // var menuButtonOpen = document.getElementsByClassName('js-swiper-mob-menu-open'),
-    // var menuButtonClose = document.getElementsByClassName('js-swiper-mob-menu-open'),
-    // var swiperMobMenu = new Swiper('.js-swiper-mob-menu .swiper-container', {
-    //   nextButton: '.js-swiper-mob-menu-open',
-    //   prevButton: '.js-swiper-mob-menu-close',
-    //   onlyExternal: true,
-    //   breakpoints: {
-    //     754: {
-    //       onlyExternal: false,
-    //       shortSwipes: false,
-    //       slideToClickedSlide: true,
-    //     }
-    //   },
-    //   onSlideChangeStart: function(slider) {
-    //     if (slider.activeIndex == 0) {
-    //       menuButtonOpen.classList.add('is-visible')
-    //     } else
-    //       menuButtonClose.classList.add('is-hidden')
-    //   },
-    //   onSlideChangeEnd: function(slider) {
-    //     if (slider.activeIndex == 1)
-    //       menuButtonOpen.classList.add('is-hidden')
-    //     else
-    //       menuButtonClose.classList.add('is-visible')
-    //   },
-    // })
-
+    }
+  })
 });
