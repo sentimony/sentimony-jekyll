6ece283078e4a6db3dca8154dbe912c00831fac9
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-08-05 18:19:21 +0300

    hide goodshare

--- _js/require.config.js
+++ _js/require.config.js
@@ -2,10 +2,6 @@ requirejs.onResourceLoad = function (context, map) {
   console.log('Loaded module: ' + map.name);
 };
 
-// requirejs.onError = function (err) {
-//   console.log(err);
-// };
-
 requirejs.config({
   baseUrl: '/assets/js/modules',
   urlArgs: '01',
@@ -15,33 +11,20 @@ requirejs.config({
     'swiper': '../lib/swiper.min',
     'lazysizes': '../lib/lazysizes.min',
     'jquery': '../lib/jquery.min',
-    'goodshare': '../lib/goodshare.min',
-    // 'common': '../common',
-    // 'app_bundle': '../bundle.min',
+    // 'goodshare': '../lib/goodshare.min',
     'swiper_mob_menu': 'swiper_mob_menu',
     'swiper_release_list': 'swiper_release_list',
     'vue_tabs': 'vue_tabs',
     'platform_add_class': 'platform_add_class',
     'platform_detector': 'platform_detector',
   },
-  // bundles: {
-  //   'app_bundle': [
-  //     'swiper_mob_menu',
-  //     'swiper_release_list',
-  //     'vue_tabs',
-  //     'platform_add_class',
-  //     'platform_detector',
-  //   ]
-  // },
   deps: [
     'vue',
     'vue-tabs',
     'swiper',
     'lazysizes',
     'jquery',
-    'goodshare',
-    // 'common',
-    // 'app_bundle',
+    // 'goodshare',
     'swiper_mob_menu',
     'swiper_release_list',
     'vue_tabs',
