7510cec0a50e37d8d9b309d425e8aabca82cbaa0
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-08-07 00:02:24 +0300

    upd ServiceWorker

---
+++ sw.js
@@ -0,0 +1,5 @@
+'use strict';
+importScripts('sw-toolbox.js');
+toolbox.precache(['index.html','assets/css/main.css']);
+toolbox.router.get('/assets/img/*', toolbox.cacheFirst);
+toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
