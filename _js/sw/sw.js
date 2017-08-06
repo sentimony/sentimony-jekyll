772c275dea32cfa03b311f17c13e23c0a0675562
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-08-06 23:48:38 +0300

    upd sw.js

--- _js/sw/sw.js
+++ _js/sw/sw.js
@@ -1,5 +1,5 @@
 "use strict";
 importScripts("sw-toolbox.js");
-toolbox.precache(["index.html","assets/css/main.css"]);
-toolbox.router.get("/assets/img/*", toolbox.cacheFirst);
-toolbox.router.get("/*", toolbox.networkFirst, { networkTimeoutSeconds: 5});
+toolbox.precache(["./index.html","./assets/css/main.css"]);
+toolbox.router.get("./assets/img/*", toolbox.cacheFirst);
+toolbox.router.get("./*", toolbox.networkFirst, { networkTimeoutSeconds: 5});
