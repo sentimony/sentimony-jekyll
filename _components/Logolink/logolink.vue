90e9f1f6b0200cc66052f2038b5081e88256a003
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-08-29 22:32:26 +0300

    cleaning

--- _components/Logolink/logolink.vue
+++ _components/Logolink/logolink.vue
@@ -3,7 +3,7 @@
     <img class="logolink__logo" src="/assets/img/svg-icons/sentimony-records-logo.svg" alt="Sentimony Records Logo SVG">
     <div class="logolink__text">
       <div class="logolink__title">{{ siteTitle }}</div>
-      <div class="logolink__slogan">{{ siteDescription }}</div>
+      <div class="logolink__description">{{ siteDescription }}</div>
     </div>
   </a>
 </template>
