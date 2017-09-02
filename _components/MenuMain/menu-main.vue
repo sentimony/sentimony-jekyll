7c7ed151843568ffaeb2009547785b0abc56eac0
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-09-03 01:27:22 +0300

    add more styles at MenuMain component

--- _components/MenuMain/menu-main.vue
+++ _components/MenuMain/menu-main.vue
@@ -2,13 +2,13 @@
   <div class="menu-main">
     <div class="menu-main__wrapper">
       <div class="menu-main__list">
-        <a class="menu-main__link" href="/">Home</a>
-        <a class="menu-main__link" href="/news/">News</a>
-        <a class="menu-main__link" href="/releases/">Releases</a>
-        <a class="menu-main__link" href="/artists/">Artists</a>
-        <a class="menu-main__link" href="/events/">Events</a>
-        <a class="menu-main__link" href="/friends/">Friends</a>
-        <a class="menu-main__link" href="/contacts/">Contacts</a>
+        <a class="menu-main__link is-selected" href="/">Home</a>
+        <a class="menu-main__link is-selected" href="/news/">News</a>
+        <a class="menu-main__link is-selected" href="/releases/">Releases</a>
+        <a class="menu-main__link is-selected" href="/artists/">Artists</a>
+        <a class="menu-main__link is-selected" href="/events/">Events</a>
+        <a class="menu-main__link is-selected" href="/friends/">Friends</a>
+        <a class="menu-main__link is-selected" href="/contacts/">Contacts</a>
       </div>
     </div>
   </div>
--- _components/MenuMain/spa-menu-main.vue
+++ _components/MenuMain/spa-menu-main.vue
@@ -2,17 +2,17 @@
   <div class="menu-main">
     <div class="menu-main__wrapper">
       <div class="menu-main__list">
-        <a class="menu-main__link" href="/">Home</a>
-        <!-- <router-link class="menu-main__link" to="/spa/">Home</router-link> -->
-        <router-link class="menu-main__link" to="/news/">News</router-link>
-        <a class="menu-main__link" href="/releases/">Releases</a>
-        <a class="menu-main__link" href="/artists/">Artists</a>
-        <a class="menu-main__link" href="/events/">Events</a>
-        <!-- <router-link class="menu-main__link" to="/spa/releases/">Releases</router-link>
-        <router-link class="menu-main__link" to="/spa/artists/">Artists</router-link>
-        <router-link class="menu-main__link" to="/spa/events/">Events</router-link> -->
-        <router-link class="menu-main__link" to="/friends/">Friends</router-link>
-        <router-link class="menu-main__link" to="/contacts/">Contacts</router-link>
+        <a class="menu-main__link is-selected" href="/">Home</a>
+        <!-- <router-link class="menu-main__link is-selected" to="/spa/">Home</router-link> -->
+        <router-link class="menu-main__link is-selected" to="/news/">News</router-link>
+        <a class="menu-main__link is-selected" href="/releases/">Releases</a>
+        <a class="menu-main__link is-selected" href="/artists/">Artists</a>
+        <a class="menu-main__link is-selected" href="/events/">Events</a>
+        <!-- <router-link class="menu-main__link is-selected" to="/spa/releases/">Releases</router-link>
+        <router-link class="menu-main__link is-selected" to="/spa/artists/">Artists</router-link>
+        <router-link class="menu-main__link is-selected" to="/spa/events/">Events</router-link> -->
+        <router-link class="menu-main__link is-selected" to="/friends/">Friends</router-link>
+        <router-link class="menu-main__link is-selected" to="/contacts/">Contacts</router-link>
       </div>
     </div>
   </div>
