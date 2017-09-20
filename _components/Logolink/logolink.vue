415cc30af4507c2c6894c2ccc8e694ee49ba222e
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-09-20 04:19:43 +0300

    rework logolink.vue & headr.vue to es6

--- _components/Logolink/logolink.vue
+++ _components/Logolink/logolink.vue
@@ -1,27 +1,23 @@
 <template>
-  <!-- <a href="/" class="logolink is-selected"> -->
   <router-link to="/" class="logolink" exact-active-class="is-selected" exact>
-    <img class="logolink__logo" src="/assets/img/svg-icons/sentimony-records-logo.svg" alt="Sentimony Records Logo SVG">
+    <img class="logolink__logo" src="/assets/img/svg-icons/sentimony-records-logo.svg" :alt="siteTitle + 'Logo SVG'">
     <div class="logolink__text">
       <div class="logolink__title">{{ siteTitle }}</div>
       <div class="logolink__description">{{ siteDescription }}</div>
     </div>
   </router-link>
-  <!-- </a> -->
 </template>
 
 <script>
-  module.exports = {
-    data: function () {
-      return {
-        siteTitle: 'Sentimony Records',
-        siteDescription: 'Psychedelic Music Label'
-      }
-    }
-  };
+export default {
+  data: () => ({
+    siteTitle: 'Sentimony Records',
+    siteDescription: 'Psychedelic Music Label'
+  })
+}
 </script>
 
 <style lang="scss?outputStyle=compressed">
-  .logolink {
-  }
+.logolink {
+}
 </style>
