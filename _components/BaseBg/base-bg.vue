74059cd53a1fb809c6b3ed6c6defaf286bb9b698
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-09-22 23:35:13 +0300

    upd base-bg.vue

--- _components/BaseBg/base-bg.vue
+++ _components/BaseBg/base-bg.vue
@@ -8,35 +8,35 @@
 </template>
 
 <script>
-  module.exports = {
-    data: function () {
-      return {
-        baseBgSrc: 'https://content.sentimony.com/assets/img/backgrounds/sombrero-red.jpg',
-        baseBgSrc1x: 'https://content.sentimony.com/assets/img/backgrounds/sombrero-red@1x.jpg 1x',
-        baseBgSrc2x: 'https://content.sentimony.com/assets/img/backgrounds/sombrero-red@2x.jpg 2x',
-        baseBgAlt: 'Sombrero Red Background'
-      }
+module.exports = {
+  data: function () {
+    return {
+      baseBgSrc: 'https://content.sentimony.com/assets/img/backgrounds/sombrero-red.jpg',
+      baseBgSrc1x: 'https://content.sentimony.com/assets/img/backgrounds/sombrero-red@1x.jpg 1x',
+      baseBgSrc2x: 'https://content.sentimony.com/assets/img/backgrounds/sombrero-red@2x.jpg 2x',
+      baseBgAlt: 'Sombrero Red Background'
     }
-  };
+  }
+};
 </script>
 
 <style lang="scss?outputStyle=compressed">
-  .base-bg {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
+.base-bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
 
-    &__img {
-      display: block;
-      width: auto;
-      height: 100%;
-      max-width: inherit;
-      position: relative;
-      left: 50%;
-      transform: translateX(-50%);
-    }
+  &__img {
+    display: block;
+    width: auto;
+    height: 100%;
+    max-width: inherit;
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
   }
+}
 </style>
