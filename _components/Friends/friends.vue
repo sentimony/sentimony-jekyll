02ed71576634f967e6d0a0bbfb69d3f21443fe44
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-09-24 00:57:05 +0300

    upd friends.vue

--- _components/Friends/friends.vue
+++ _components/Friends/friends.vue
@@ -2,7 +2,7 @@
   <div class="friends">
     <h1>Friends</h1>
     <div class="friends__list">
-      <p v-for="i in friends.data" class="friends__item">
+      <p v-for="i in friends" class="friends__item">
         <span class="friends__title">{{ i.title }}</span>
         <a v-if="i.site" :href="i.site" class="friends__site"><span class="icon-public"></span></a>
         <a v-if="i.bandcamp" :href="i.bandcamp" class="friends__link"><span class="icon-bandcamp"></span></a>
