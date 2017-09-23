90b1382fe12efe63b1bb13145bfabd815c32b050
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-09-24 00:57:18 +0300

    upd Events

--- _components/EventPage/event-page.vue
+++ _components/EventPage/event-page.vue
@@ -10,60 +10,60 @@
 </template>
 
 <script>
-  // var SvgTriangle = require('./svg-triangle.vue');
-  // var {VueTabs, VTab} = require('../js/vue-tabs.min.js');
-  // var DisqusComments = require('./disqus-comments.vue');
+// var SvgTriangle = require('./svg-triangle.vue');
+// var {VueTabs, VTab} = require('../js/vue-tabs.min.js');
+// var DisqusComments = require('./disqus-comments.vue');
 
-  module.exports = {
-    components: {
-      // 'svg-triangle': SvgTriangle,
-      // 'vue-tabs': VueTabs,
-      // 'v-tab': VTab,
-      // 'disqus-comments': DisqusComments
-    },
-    data: function () {
-      return {
-        event: {
-          data: [
-            {
-              title: '',
-              date: '',
-              url: ''
-            }
-          ]
-        }
+module.exports = {
+  components: {
+    // 'svg-triangle': SvgTriangle,
+    // 'vue-tabs': VueTabs,
+    // 'v-tab': VTab,
+    // 'disqus-comments': DisqusComments
+  },
+  data: function () {
+    return {
+      event: {
+        data: [
+          {
+            title: '',
+            date: '',
+            url: ''
+          }
+        ]
       }
-    },
-    created: function () {
-      this.fetchData();
-    },
-    watch: {
-      '$route': 'fetchData'
-    },
-    methods: {
-      fetchData: function () {
-        var self = this;
-        var id = this.$route.params.slug;
-        this.axios.get('https://sentimony-db.firebaseio.com/events/data/' + id + '.json').then(function (response) {
-          self.event = response.data;
-          console.log('firebase: EVENT ' + id + ' catched');
-        }).catch(function (error) {
-          console.log(error);
-        });
-      }
-    },
-    filters: {
-      formatDate: function (date) {
-        var moment = require('moment');
-        if (date) {
-          return moment(String(date)).format('DD MMM YYYY');
-        }
+    }
+  },
+  created: function () {
+    this.fetchData();
+  },
+  watch: {
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData: function () {
+      var self = this;
+      var id = this.$route.params.slug;
+      this.axios.get('https://sentimony-db.firebaseio.com/events/' + id + '.json').then(function (response) {
+        self.event = response.data;
+        console.log('firebase: EVENT ' + id + ' catched');
+      }).catch(function (error) {
+        console.log(error);
+      });
+    }
+  },
+  filters: {
+    formatDate: function (date) {
+      var moment = require('moment');
+      if (date) {
+        return moment(String(date)).format('DD MMM YYYY');
       }
     }
   }
+}
 </script>
 
 <style lang="scss?outputStyle=compressed">
-  .event-page {
-  }
+.event-page {
+}
 </style>
--- _components/Events/events.vue
+++ _components/Events/events.vue
@@ -3,7 +3,6 @@
     <h1>Events</h1>
     <div class="events__list">
       <p v-for="i in sortByDate" class="events__item">
-        <!-- <a :href="i.url" class="events__link" v-if="i.date" v-html="i.date + ' @ ' + i.title"></a> -->
         <router-link :to="i.url" class="events__link" v-if="i.date">{{ i.date | formatDate }} @ {{ i.title }}</router-link>
       </p>
     </div>
@@ -11,49 +10,48 @@
 </template>
 
 <script>
-  var sortBy = require('lodash/sortBy');
+var sortBy = require('lodash/sortBy');
 
-  module.exports = {
-    data: function () {
-      return {
-        events: {
-          data: [
-            {
-              title: '',
-              date: '',
-              url: ''
-            }
-          ]
-        }
-      }
-    },
-    created: function () {
-      var self = this;
-      this.axios.get('https://sentimony-db.firebaseio.com/events.json').then(function (response) {
-        self.events = response.data;
-        console.log('firebase: EVENTS catched');
-      }).catch(function (error) {
-        console.log(error);
-      });
-    },
-    filters: {
-      formatDate: function (date) {
-        var moment = require('moment');
-        if (date) {
-          return moment(String(date)).format('DD MMM YYYY');
-        }
-      }
-    },
-    computed: {
-      sortByDate () {
-        return sortBy(this.events.data, 'date').reverse()
+module.exports = {
+  data: function () {
+    return {
+      events: {
+        data: [
+          {
+            title: '',
+            date: '',
+            url: ''
+          }
+        ]
       }
     }
-
-  };
+  },
+  created: function () {
+    var self = this;
+    this.axios.get('https://sentimony-db.firebaseio.com/events.json').then(function (response) {
+      self.events = response.data;
+      console.log('firebase: EVENTS catched');
+    }).catch(function (error) {
+      console.log(error);
+    });
+  },
+  filters: {
+    formatDate: function (date) {
+      var moment = require('moment');
+      if (date) {
+        return moment(String(date)).format('DD MMM YYYY');
+      }
+    }
+  },
+  computed: {
+    sortByDate () {
+      return sortBy(this.events, 'date').reverse()
+    }
+  }
+};
 </script>
 
 <style lang="scss?outputStyle=compressed">
-  .events {
-  }
+.events {
+}
 </style>
