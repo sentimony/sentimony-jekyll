0d0d827ff6a5ca0aa7886991b7897d8b053d425d
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-09-24 00:34:47 +0300

    rework news

--- _components/News/news.vue
+++ _components/News/news.vue
@@ -2,8 +2,7 @@
   <div class="news">
     <h1>News</h1>
     <div class="news__list">
-      <p v-for="i in news.data" class="news__item">
-        <!-- <a :href="i.url" class="news__link" v-if="i.date" v-html="i.date + ' @ ' + i.title"></a> -->
+      <p v-for="i in news" class="news__item">
         <router-link :to="i.url" class="news__link" v-if="i.date" v-html="">{{ i.date | formatDate }} @ {{ i.title }}</router-link>
       </p>
     </div>
@@ -11,41 +10,39 @@
 </template>
 
 <script>
-  module.exports = {
-    data: function () {
-      return {
-        news: {
-          data: [
-            {
-              title: '',
-              date: '',
-              url: ''
-            }
-          ]
-        }
-      }
-    },
-    created: function () {
-      var self = this;
-      this.axios.get('https://sentimony-db.firebaseio.com/news.json').then(function (response) {
-        self.news = response.data;
-        console.log('firebase: NEWS catched');
-      }).catch(function (error) {
-        console.log(error);
-      });
-    },
-    filters: {
-      formatDate: function (date) {
-        var moment = require('moment');
-        if (date) {
-          return moment(String(date)).format('DD MMM YYYY');
-        }
+module.exports = {
+  data: function () {
+    return {
+      news: [
+          {
+            title: '',
+            date: '',
+            url: ''
+          }
+        ]
+    }
+  },
+  created: function () {
+    var self = this;
+    this.axios.get('https://sentimony-db.firebaseio.com/news.json').then(function (response) {
+      self.news = response.data;
+      console.log('firebase: NEWS catched');
+    }).catch(function (error) {
+      console.log(error);
+    });
+  },
+  filters: {
+    formatDate: function (date) {
+      var moment = require('moment');
+      if (date) {
+        return moment(String(date)).format('DD MMM YYYY');
       }
     }
-  };
+  }
+};
 </script>
 
 <style lang="scss?outputStyle=compressed">
-  .news {
-  }
+.news {
+}
 </style>
